0861901ac44f3925f1ec952b4671b515987e6d26
rajvir singh dev <devrajvirsingh@gmail.com>
2024-01-18 19:30:56 +0530

    my new commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3300;
-const routes = require('./routes')
-const cors = require('cors')
+const port = 3000;
+const routes = require("./routes");
+const cors = require("cors");
 
-app.use(cors())
-app.use(routes)
-app.listen(port , ()=>{
-    console.log(`server is listening on ${port}`)
-})
+app.use(cors());
+app.use("/api", routes);
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
--- routes.js
+++ routes.js
@@ -8,23 +8,12 @@ const bestseller = require("./bestseller.json");
 router.get("/featured", (req, res) => {
   res.send(f_list);
 });
-router.get("/featured/:id", (req, res) => {
-  const fl = f_list.find((item) => item.id == req.params.id);
-  res.send(fl);
-});
+
 router.get("/combo", (req, res) => {
   res.send(combo);
 });
-router.get("/combo/:id", (req, res) => {
-  const combos = combo.find((item) => item.id == req.params.id);
-  res.send(combos);
-});
 router.get("/bestseller", (req, res) => {
   res.send(bestseller);
-});
-router.get("/bestseller/:id", (req, res) => {
-  const bs = bestseller.find((item) => item.id == req.params.id);
-  res.send(bs);
 });
 
 module.exports = router;
